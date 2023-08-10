3624cb132cdf6776f2ef2c350d66f5929dbbc8d9
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-08-10 14:35:45 +0900

    [BE] fix:  cors 허용 메서드 추가

--- be/issue-tracker/src/main/java/org/presents/issuetracker/global/config/WebConfig.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/global/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("*", "http://localhost:5173")
-			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS");
 	}
 }
